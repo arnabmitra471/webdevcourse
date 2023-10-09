8551e70d6276a97761603d4b22a7d2eceb089ed6
Arnab <arnabmitra471@gmail.com>
2023-10-09 20:31:43 +0530

    An example of if else if else statement

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+let age = 28
+if(age >0 && age <5)
+{
+    console.log("You are a kid and you should go to kidergarten")
+}
+else if(age >5 && age < 9)
+{
+    console.log("You should go to school now")
+}
+else if(age >9 && age <18)
+{
+    console.log("You can think of driving when you turn 18")
+}
+else
+{
+    console.log("You can drive");
+}
